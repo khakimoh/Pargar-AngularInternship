1d36fbc65d24a2d03f6152fce135cd47d0b0a9bc
khaki mohammad <mimkhaki+github@gmail.com>
2021-09-10 12:36:29 +0430

    add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page404Component } from './extra/components/error/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ()=> import('./modules/home/home.module').then(mod => mod.HomeModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: ()=> import('./modules/profile/profile.module').then(mod => mod.ProfileModule)
+  },
+  {
+    path: '**',
+    component: Page404Component
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page404Component } from './extra/components/error/page404/page404.component';
+import { HeaderComponent } from './extra/components/template/header/header.component';
+import { FooterComponent } from './extra/components/template/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
