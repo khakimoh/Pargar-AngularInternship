ca1261f6c70a0927c73b558ddfe1df5d782f66a1
khaki mohammad <mimkhaki+github@gmail.com>
2021-09-10 12:01:05 +0430

    add another module

---
+++ src/app/modules/catagory/catagory.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CatagoryRoutingModule } from './catagory-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CatagoryRoutingModule
+  ]
+})
+export class CatagoryModule { }
