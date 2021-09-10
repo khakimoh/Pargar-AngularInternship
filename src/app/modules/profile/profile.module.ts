8b4435f783c1d860b1fd98fb05052b61deac21f6
khaki mohammad <mimkhaki+github@gmail.com>
2021-09-10 12:10:38 +0430

    add profile component

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileInfoComponent
+  ],
   imports: [
     CommonModule,
     ProfileRoutingModule
